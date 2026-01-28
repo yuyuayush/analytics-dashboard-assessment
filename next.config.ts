0fab90d6418a8687825e111035d5fc299cda3fcd
ayush negi <ayushnegi1912@gmail.com>
2026-01-28 15:47:52 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
-  basePath: "/",
+  basePath: "",
   reactCompiler: true,
   images: {
     unoptimized: true,
